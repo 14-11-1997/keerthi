9380fd8dacb17ace6ce6b4824203cb78f6540ad3
14-11-1997 <kirthikas.cse@gmail.com>
2016-12-26 01:27:03 -0800

    Add files via upload

---
+++ evodd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int t;
+printf("\n enter the number");
+scanf("%d",&t);
+if(t%2!=0)
+printf("the value of t is odd");
+else
+printf("the value of t is even");
+}
